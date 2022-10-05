75bfa11195388cc8490a9149a28572488319b118
Chukki-G <115051259+Chukki-G@users.noreply.github.com>
2022-10-05 14:52:09 -0500

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,16 +2,10 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 	
-	@GetMapping("/status")
-	public String deploy() {
-		return "Hurray !!! Application deployed to AWS... !!!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
