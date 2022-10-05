b992c3f13ac542c8d4d350730c30cce40eef2ef1
Chukki-G <115051259+Chukki-G@users.noreply.github.com>
2022-10-05 14:52:44 -0500

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ public class HelloWorldController {
 public String hello() {
 	return "Hello World!!!";
 }
+
+@GetMapping("/status")
+public String deploy() {
+	return "Hurray !!! Application deployed to AWS... !!!";
+}
+
 }
