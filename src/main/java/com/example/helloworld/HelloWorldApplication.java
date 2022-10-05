774bd59d92df11ff58f614608eb4de0cb55acc5c
Chukki-G <115051259+Chukki-G@users.noreply.github.com>
2022-10-05 14:37:09 -0500

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+	
+	@GetMapping("/status")
+	public String deploy() {
+		return "Hurray !!! Application deployed to AWS... !!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
